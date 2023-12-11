3d9e7f31bedb2595f5d2488f9d6a44e60c4e92b6
schlibra <schlibra@163.com>
2023-12-11 15:23:14 +0800

    Fix some path

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 $action = @$_REQUEST["action"] ?? "";
 $token = @$_REQUEST["token"] ?? "";
-if($token != file_get_contents("token")){
+if($token != file_get_contents("../token")){
     die(json_encode(["code"=>0,"msg"=>"你没有权限访问该页面"]));
 }
 switch ($action){
@@ -22,6 +22,8 @@ switch ($action){
     case "init":
         initData();
         break;
+    default:
+        echo json_encode(["code"=>0,"msg"=>"没有指定操作"]);
 }
 function importData(){
     $type = @$_GET["type"] ?? "";
@@ -34,9 +36,9 @@ function importData(){
             if (findRepeat(["title"=>$title,"answer"=>$answer])){
                 echo json_encode(["code"=>0,"msg"=>"题目已存在，跳过导入"]);
             }else{
-                $data = json_decode(file_get_contents("data.json"),true);
+                $data = json_decode(file_get_contents("../data.json"),true);
                 $data[] = ["title"=>$title,"answer"=>$answer];
-                file_put_contents("data.json",json_encode($data));
+                file_put_contents("../data.json",json_encode($data));
                 echo json_encode(["code"=>1,"msg"=>"导入成功"]);
             }
         }
@@ -46,7 +48,7 @@ function importData(){
             echo json_encode(["code"=>0,"msg"=>"字段不能为空"]);
         }else{
             $import_data = json_decode($data,true);
-            $data = json_decode(file_get_contents("data.json"),true);
+            $data = json_decode(file_get_contents("../data.json"),true);
             $count = 0;
             for ($i=0;$i<count($import_data);++$i){
                 $item = $import_data[$i];
@@ -57,7 +59,7 @@ function importData(){
                     }
                 }
             }
-            file_put_contents("data.json",json_encode($data));
+            file_put_contents("../data.json",json_encode($data));
             echo json_encode(["code"=>1,"msg"=>"导入完成，传入".count($import_data)."道题，有效导入{$count}道题"]);
         }
     }else{
@@ -72,9 +74,9 @@ function updateData(){
     if ($id === "" || empty($title) || empty($answer)){
         die(json_encode(["code"=>0,"msg"=>"字段不能为空"]));
     }
-    $data = json_decode(file_get_contents("data.json"),true);
+    $data = json_decode(file_get_contents("../data.json"),true);
     $data[$id] = ["title"=>$title, "answer"=>$answer];
-    file_put_contents("data.json", json_encode($data));
+    file_put_contents("../data.json", json_encode($data));
     echo json_encode(["code"=>1,"msg"=>"数据更新成功"]);
 }
 
@@ -85,7 +87,7 @@ function deleteData(){
     if ($id === "" || empty($title) || empty($answer)){
         die(json_encode(["code"=>0,"msg"=>"字段不能为空"]));
     }
-    $data = json_decode(file_get_contents("data.json"),true);
+    $data = json_decode(file_get_contents("../data.json"),true);
     $count = count($data);
     if ($data[$id]["title"] === $title and $data[$id]["answer"] === $answer){
         $new_data = [];
@@ -97,7 +99,7 @@ function deleteData(){
         if (count($new_data) === $count){
             echo json_encode(["code"=>0,"msg"=>"数据更新失败，请重试"]);
         }else{
-            file_put_contents("data.json",json_encode($new_data));
+            file_put_contents("../data.json",json_encode($new_data));
             echo json_encode(["code"=>1,"msg"=>"数据删除成功"]);
         }
     }else{
@@ -105,15 +107,15 @@ function deleteData(){
     }
 }
 function getData(){
-    echo json_encode(["code"=>1,"data"=>json_decode(file_get_contents("data.json"),true)]);
+    echo json_encode(["code"=>1,"data"=>json_decode(file_get_contents("../data.json"),true)]);
 }
 function initData(){
-    file_put_contents("data.json",json_encode([]));
+    file_put_contents("../data.json",json_encode([]));
     echo json_encode(["code"=>1,"msg"=>"数据初始化成功"]);
 }
 function findRepeat($item): bool
 {
-    $data = json_decode(file_get_contents("data.json"),true);
+    $data = json_decode(file_get_contents("../data.json"),true);
     for($i=0;$i<count($data);++$i){
         if ($data[$i]["title"]==$item["title"]){
             return true;
