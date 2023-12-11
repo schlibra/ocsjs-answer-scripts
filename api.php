6e56a612ad14219d290439386fb53e155b7f1e46
schlibra <schlibra@163.com>
2023-12-11 10:26:44 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,36 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
-echo json_encode([
-    "code"=> 1,
-    "msg" => "请求成功"
-]);
+$action = @$_REQUEST["action"] ?? "";
+$token = @$_REQUEST["token"] ?? "";
+if($token != file_get_contents("token")){
+    die(json_encode(["code"=>0,"msg"=>"你没有权限访问该页面"]));
+}
+switch ($action){
+    case "import":
+        importData();
+        break;
+    case "update":
+        updateData();
+        break;
+    case "delete":
+        deleteData();
+        break;
+    case "get":
+        getData();
+        break;
+}
+function importData(){
+
+}
+
+function updateData(){
+
+}
+
+function deleteData(){
+
+}
+function getData(){
+    readfile("data.json");
+}
