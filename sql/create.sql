67c2639dfec9729b434eaea863ca3cd9a90219e9
schlibra <schlibra@163.com>
2023-12-14 15:38:47 +0800

    Add sql file

---
+++ sql/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "data"
+(
+    "id"     INT PRIMARY KEY,
+    "title"  TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "work"   TEXT NOT NULL,
+    "course" TEXT NOT NULL,
+    "create" TEXT NOT NULL,
+    "update" TEXT NOT NULL
+);
