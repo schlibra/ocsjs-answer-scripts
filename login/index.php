b647f8cd6e59bc6e27de09e666c1e77894b1a917
schlibra <schlibra@163.com>
2023-12-23 21:25:49 +0800

    Add login page

---
+++ login/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <title>题库数据管理 - 登录</title>
+    <meta charset="UTF-8"/>
+    <link href="https://cdn.tsinbei.com/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <h1 class="center">题库数据管理管理</h1>
+    <h3>登录</h3>
+</div>
+<script src="https://cdn.tsinbei.com/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/mhDoLbDldZc3qpsJHpLogda//BVZbgYuw6kof4u2FrCedxOtgRZDTHgHUhOCVim"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.tsinbei.com/npm/sweetalert"></script>
+<!--<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>-->
+<script src="https://cdn.tsinbei.com/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+<script>
+
+</script>
+</body>
+</html>
