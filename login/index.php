5196d31562e7efebd0d637f4c9b428ed0471975e
schlibra <schlibra@163.com>
2023-12-25 09:45:49 +0800

    Update login

--- login/index.php
+++ login/index.php
@@ -1,19 +1,41 @@
 <!DOCTYPE html>
-<html lang="zh">
+<html lang="zh" data-bs-theme="dark">
 <head>
     <title>题库数据管理 - 登录</title>
     <meta charset="UTF-8"/>
     <link href="https://cdn.tsinbei.com/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+          crossorigin="anonymous">
 
 </head>
 <body>
 <div class="container">
-    <h1 class="center">题库数据管理管理</h1>
-    <h3>登录</h3>
+    <h1 class="text-center mt-4">题库数据管理</h1>
+    <h2 class="text-center mt-2">登录</h2>
+    <div class="row">
+        <div class="col"></div>
+        <form class="col">
+            <div class="mt-3">
+                <label for="username" class="form-label">用户名：</label>
+                <input type="text" id="username" class="form-control">
+            </div>
+            <div class="mt-3">
+                <label for="password" class="form-label">密码：</label>
+                <input type="password" id="password" class="form-control">
+            </div>
+            <div class="row mt-3">
+                <div class="col"></div>
+                <div class="col text-center">
+                    <button type="button" class="btn btn-lg btn-primary">登录</button>
+                </div>
+                <div class="col"></div>
+            </div>
+        </form>
+        <div class="col"></div>
+    </div>
+
 </div>
 <script src="https://cdn.tsinbei.com/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-/mhDoLbDldZc3qpsJHpLogda//BVZbgYuw6kof4u2FrCedxOtgRZDTHgHUhOCVim"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
 <script src="https://cdn.tsinbei.com/npm/sweetalert"></script>
 <!--<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>-->
